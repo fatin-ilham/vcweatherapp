4152c766ecdcf0334d8f5f3c13f2b0e0cfe3eae2
Fatin Ilham <fatin.ilham@g.bracu.ac.bd>
2025-06-15 15:59:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 
 async function getWeather() {
   const city = document.getElementById("cityInput").value.trim();
